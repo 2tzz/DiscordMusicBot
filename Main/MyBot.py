51ebe45007d2706078ac99ff9064bbf2083b7967
2tzz <157218559+2tzz@users.noreply.github.com>
2025-03-12 19:29:53 +0530

    frist testing done worked successfully

--- Main/MyBot.py
+++ Main/MyBot.py
@@ -160,7 +160,7 @@ async def play_next_song(voice_client, guild_id, channel):
 
         ffmpeg_options = {
             "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-            "options": "-vn -c:a libopus -b:a 96k",
+            "options": "-vn -c:a libopus -b:a 256k",
             # Remove executable if FFmpeg is in PATH
         }
 
---
+++ Main/test.py
@@ -0,0 +1,9 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+import yt_dlp # NEW
+from collections import deque # NEW
+import asyncio # NEW
+
